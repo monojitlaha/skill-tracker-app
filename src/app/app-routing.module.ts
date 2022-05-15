ac2893c4b6511a0574a30cc3eb5f6534cddc72bf
monojit.laha@gmail.com <monojit.laha@gmail.com>
2022-05-15 14:49:51 +0530

    Added Routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '',  redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', pathMatch: 'full', component: LoginComponent},
+  { path: 'profile', pathMatch: 'full', component: ProfileComponent} 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
