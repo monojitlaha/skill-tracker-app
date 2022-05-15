591ea178c6bfd67af75ddba7c6e6ed94528fd629
monojit.laha@gmail.com <monojit.laha@gmail.com>
2022-05-15 14:25:33 +0530

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { LoginComponent } from './components/login/login.component';
+import { ProfileService } from './services/profile.service';
+import { LoginService } from './services/login.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [ProfileService, LoginService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
