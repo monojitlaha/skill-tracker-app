577660d73319975a1cec30aeddd66da3c8c63b69
monojit.laha@gmail.com <monojit.laha@gmail.com>
2022-05-15 16:51:30 +0530

    Added Routing Navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { ProfileComponent } from './components/profile/profile.component';
+import { AuthenticationGuard } from './guards/authentication.guard';
 
 const routes: Routes = [
   { path: '',  redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', pathMatch: 'full', component: LoginComponent},
-  { path: 'profile', pathMatch: 'full', component: ProfileComponent} 
+  { path: 'profile', pathMatch: 'full', component: ProfileComponent, canActivate:[AuthenticationGuard]} 
 ];
 
 @NgModule({
